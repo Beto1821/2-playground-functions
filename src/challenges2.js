9a8946130c4c326d102e7cc309da87d4632d269b
Beto1821 <beto1821@uol.com.br>
2022-03-12 20:51:27 -0300

    fullback ex11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,74 +1,17 @@
-/* eslint-disable operator-linebreak */
-/* eslint-disable no-undef */
-/* eslint-disable sonarjs/cognitive-complexity */
-/* eslint-disable no-else-return */
-/* eslint-disable max-lines-per-function */
-/* eslint-disable complexity */
 // Desafio 11
-function generatePhoneNumber(list) {
-  let neg = 0;
-  let cont = 0;
-  if (list.length != 11) {
-    return 'Array com tamanho incorreto.';
-  }
-
-  for (i of list) {
-    let rep = 0;
-    for (z of list) {
-      if (i === z) {
-        rep += 1;
-        if (rep > 3) {
-          cont += 1;
-        }
-      }
-    }
-  }
-
-  for (i of list) {
-    if (list[i] < 0 || list[i] > 9) {
-      neg += 1;
-    }
-  }
-  if (neg > 0 || cont > 0) {
-    return 'não é possível gerar um número de telefone com esses valores';
-  }
-
-  return (
-    '(' +
-    list[0] +
-    '' +
-    list[1] +
-    ') ' +
-    list[2] +
-    '' +
-    list[3] +
-    '' +
-    list[4] +
-    '' +
-    list[5] +
-    '' +
-    list[6] +
-    '-' +
-    list[7] +
-    '' +
-    list[8] +
-    '' +
-    list[9] +
-    '' +
-    list[10]
-  );
+function generatePhoneNumber() {
+  // seu código aqui
 }
 
+// Desafio 12
+function triangleCheck() {
+  // seu código aqui
+}
 
-// // Desafio 12
-// function triangleCheck() {
-//   // seu código aqui
-// }
-
-// // Desafio 13
-// function hydrate() {
-//   // seu código aqui
-// }
+// Desafio 13
+function hydrate() {
+  // seu código aqui
+}
 
 module.exports = {
   generatePhoneNumber,
