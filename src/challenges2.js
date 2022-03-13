664051438e61e27bc92e644de04ce5fb66bc18e5
Beto1821 <beto1821@uol.com.br>
2022-03-12 23:32:07 -0300

    ex13 rev00

--- src/challenges2.js
+++ src/challenges2.js
@@ -28,18 +28,24 @@ function generatePhoneNumber(list) {
     return 'não é possível gerar um número de telefone com esses valores';
   }
 
-  return (`(${list[0]}${list[1]}) ${list[2]}${list[3]}${list[4]}${list[5]}${list[6]}-${list[7]
-  }${list[8]}${list[9]}${list[10]}`);
+  return `(${list[0]}${list[1]}) ${list[2]}${list[3]}${list[4]}${list[5]}${list[6]}-${list[7]}${list[8]}${list[9]}${list[10]}`;
 }
 
 // Desafio 12
 function triangleCheck(a, b, c) {
-  return (a < b + c && b < a + c && c < a + b);
+  return a < b + c && b < a + c && c < a + b;
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(frase) {
+  let soma = 0;
+  let numbers = frase.match(/\d+/g);
+  for (let i = 0; i < numbers.length; i += 1) {
+    soma += parseInt(numbers[i]);
+  }
+  if (numbers.length === 1) {
+    return '1 copo de água';
+  } return `${soma} copos de água`;
 }
 
 module.exports = {
